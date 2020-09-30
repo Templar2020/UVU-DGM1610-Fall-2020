95ea6c67ef858880f74dde2e2fe6276edf79fe72
Preston Powell <preston.powell2020@gmail.com>
2020-09-30 17:28:52 -0600

    2.3 - Animal Spawn System

--- Prototype 2 - Feed the Animals/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2 - Feed the Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,20 +5,16 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     public float topBounds = 30f;
-    public float lowerBounds = -10f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public float lowerBounds = -10.0f;
+    
     // Update is called once per frame
     void Update()
     {
+        // Destroy any object entering the topbounds
         if(transform.position.z > topBounds){
             Destroy(gameObject);
         }
+        // Destory any object entering the lowerbounds
         else if(transform.position.z < lowerBounds)
         {
             Destroy(gameObject);
