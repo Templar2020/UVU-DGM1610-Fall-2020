5784bb6cd6944e3adf9f554b160772b1172ef596
Preston Powell <preston.powell2020@gmail.com>
2020-10-14 17:10:40 -0600

    Finished 3.1 Run & Mech, Obstacle movement, Jump limitation

---
+++ Prototype 3 - Run & Jump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    public float jumpForce;
+    public float gravityMod;
+
+    private bool isGrounded = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true) 
+       {
+           isGrounded = false;
+           playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+           
+       }    
+      
+    }
+    void OnCollisionEnter(Collision other)
+       {
+           
+           isGrounded = true;
+       }
+}
